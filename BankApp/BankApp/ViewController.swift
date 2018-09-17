d473603399d3a86ca22a2fb73e5e84d89cff20ac
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-09-17 09:27:26 -0400

    inital commit

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Flavius Bortas on 9/17/18.
+//  Copyright © 2018 KBFL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
