f9fa5b210d6f5714a28a232ad6e3666002a1db01
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-09-17 14:04:08 -0400

    Created and connected ViewControllers to views

---
+++ BankApp/BankApp/CheckingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CheckingViewController.swift
+//  BankApp
+//
+//  Created by Kendall Poindexter on 9/17/18.
+//  Copyright © 2018 KBFL. All rights reserved.
+//
+
+import UIKit
+
+class CheckingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
